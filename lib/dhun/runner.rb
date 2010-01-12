253f5d99cf36b05dba521501b5739bf450440ac1
achiu <mr.arthur.chiu@gmail.com>
2010-01-12 17:23:59 +0800

    neater help display with example usage

--- lib/dhun/runner.rb
+++ lib/dhun/runner.rb
@@ -35,7 +35,8 @@ module Dhun
       say "Stopping Dhun", :green
     end
 
-    desc "query SEARCH","Show files matching query. See docs for details on query syntax"
+    desc "query SEARCH","Show files matching query. ex: dhun query bob,'the marley'
+    \t\t\t See docs for details on query syntax"
     method_option :artist, :type => :string, :aliases => '-a'
     method_option :album, :type => :string, :aliases => '-l'
     method_option :genre, :type => :string, :aliases => '-g'
@@ -65,7 +66,8 @@ module Dhun
       files
     end
 
-    desc "play SEARCH","Play songs matching query. See docs for details on query syntax"
+    desc "play SEARCH","Play songs matching query. ex: dhun play bob,'the marley'
+    \t\t\t See docs for details on query syntax"
     method_option :artist, :type => :string, :aliases => '-a'
     method_option :album, :type => :string, :aliases => '-l'
     method_option :genre, :type => :string, :aliases => '-g'
@@ -76,7 +78,8 @@ module Dhun
       invoke :enqueue, [search], options
     end
 
-    desc "enqueue SEARCH","Enqueue songs matching query. See docs for details on query syntax"
+    desc "enqueue SEARCH","Enqueue songs matching query. ex: dhun enqueue bob,'the marley'
+    \t\t\t See docs for details on query syntax"
     method_option :artist, :type => :string, :aliases => '-a'
     method_option :album, :type => :string, :aliases => '-l'
     method_option :genre, :type => :string, :aliases => '-g'
