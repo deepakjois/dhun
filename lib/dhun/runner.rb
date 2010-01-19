90591763041916449a49837c878a5344c95b401e
achiu <mr.arthur.chiu@gmail.com>
2010-01-19 15:28:24 -0800

    runner tests and moved all private runner methods into no_tasks block

--- lib/dhun/runner.rb
+++ lib/dhun/runner.rb
@@ -46,7 +46,7 @@ module Dhun
       search = search.nil? ? nil : search.split(',')
       query = Dhun::Query.new(search,options)
       if query.is_valid?
-        
+
         #make the prompt pretty. i think.
         opts = options.collect {|field,value| "#{field}:#{value}" }.join(" ")
         term = search.nil? ? '[nil]' : search.join(",")
@@ -87,16 +87,16 @@ module Dhun
     method_option :file, :type => :string, :aliases => '-f'
     method_option :title, :type => :string, :aliases => '-t'
     def enqueue(search=nil)
-      
+
       # invoke query command and return us all the files found.
       files = invoke :query, [search], options
       if files and !files.empty?
         indexes = files.size == 1 ? [0] : enqueue_prompt(files.size)
-        
+
         selected = indexes.map { |index| files[index.to_i] }
         say "selected:",:green
         say_list selected
-        
+
         return_response(:enqueue,nil,selected)
       end
     end
@@ -105,7 +105,7 @@ module Dhun
     def next(count=1)
       return_response(:next,[],count.to_i)
     end
-    
+
     desc "prev COUNT", "Skips to previous song by COUNT"
     def prev(count=1)
       return_response(:prev,[],count.to_i)
@@ -113,8 +113,8 @@ module Dhun
 
     desc "status", "Shows the status"
     def status
-      return unless server_running?
       response = return_response(:status,[:current,:queue])
+      return false unless response
       say "Currently Playing:",:magenta
       say response[:current],:white
       say "Queue:",:cyan
@@ -124,101 +124,100 @@ module Dhun
     desc "history", "Shows the previously played songs"
     def history
       response = return_response(:history,[:history])
-      if response[:history]
-        say "History:",:cyan
-        say_list response[:history]
-      end
+      return false unless response
+      say "History:",:cyan
+      say_list response[:history]
     end
-    
+
     desc "shuffle", "Shuffles the queue"
     def shuffle
       response = return_response(:shuffle,[:queue])
-      if response[:queue]
-        say "Queue:",:cyan
-        say_list response[:queue]
-      end
+      return false unless response
+      say "Queue:",:cyan
+      say_list response[:queue]
     end
 
     desc "pause", "Pauses playing"
     def pause
       return_response(:pause,[])
     end
-    
+
     desc "resume", "Resumes playing"
     def resume
       return_response(:resume,[])
     end
-    
+
     desc "stop", "Stops playing"
     def stop
       return_response(:stop,[])
     end
 
+    no_tasks do
 
-    private
-
-    #prompt for enqueue
-    def enqueue_prompt(size)
-      answer = ask "Enter index to queue (ENTER to select all): ",:yellow
-      case
-      when answer.include?(',')
-        return answer.split(',')
-      when answer.include?(' ')
-        return answer.split(' ')
-      when answer.size >= 1
-        return answer.to_a
-      else
-        return 0..(size - 1)
-      end
-    end
-
-    # sends command to dhun client
-    def send_command(command,arguments=[])
-      cmd = { "command" => command.to_s, "arguments" => arguments }.to_json
-      send_message(cmd,"/tmp/dhun.sock")
-    end
-
-    # send command to the server and retrieve response.
-    def get_response(command,arguments=[])
-      if server_running?
-          resp = send_command(command,arguments)
-          return Dhun::Result.from_json_str(resp)
-      end
-    end
-    
-    # prints out list with each index value
-    # in pretty format! (contrasting colors)
-    def say_list(list)
-      list.each_with_index do |item,index|
-        color = index.even? ? :white : :cyan
-        say("#{index} : #{item}",color)
-      end
-    end
-
-    # check to see if Dhun Server is running.
-    # asks to start Dhun server if not
-    # takes argument :silent to quiet its output.
-    # need to make the socket choices more flexible
-    def server_running?(socket = "/tmp/dhun.sock",verbose = :verbose)
-      socket ||= "/tmp/dhun.sock"
-      if is_server?(socket)
-        return true
-      else
-        say("Please start Dhun server first with : dhun start_server", :red) unless verbose == :silent
-        return false
-      end
-    end
-    
-    #send out the command to server and see what it has to say.
-    def return_response(action,keys,argument=[])
-      response = get_response(action,argument)
-      if response
-        color = response.success? ? :red : :cyan
-        say response[:message], color
-        if keys
-          return keys.inject({}) {|base,key| base[key.to_sym] = response[key.to_sym] ; base}
+      #send out the command to server and see what it has to say.
+      def return_response(action,keys,argument=[])
+        response = get_response(action,argument)
+        if response
+          color = response.success? ? :red : :cyan
+          say response[:message], color
+          if keys
+            return keys.inject({}) {|base,key| base[key.to_sym] = response[key.to_sym] ; base}
+          end
         end
       end
+
+      # send command to the server and retrieve response.
+      def get_response(command,arguments=[])
+        if server_running?
+          resp = send_command(command,arguments)
+          return Dhun::Result.from_json_str(resp)
+        end
+      end
+
+      # sends command to dhun client
+      def send_command(command,arguments=[])
+        cmd = { "command" => command.to_s, "arguments" => arguments }.to_json
+        send_message(cmd,"/tmp/dhun.sock")
+      end
+
+      # check to see if Dhun Server is running.
+      # asks to start Dhun server if not
+      # takes argument :silent to quiet its output.
+      # need to make the socket choices more flexible
+      def server_running?(socket = "/tmp/dhun.sock",verbose = :verbose)
+        socket ||= "/tmp/dhun.sock"
+        if is_server?(socket)
+          return true
+        else
+          say("Please start Dhun server first with : dhun start_server", :red) unless verbose == :silent
+          return false
+        end
+      end
+
+      #prompt for enqueue
+      def enqueue_prompt(size)
+        answer = ask "Enter index to queue (ENTER to select all): ",:yellow
+        case
+        when answer.include?(',')
+          return answer.split(',')
+        when answer.include?(' ')
+          return answer.split(' ')
+        when answer.size >= 1
+          return answer.to_a
+        else
+          return 0..(size - 1)
+        end
+      end
+
+      # prints out list with each index value
+      # in pretty format! (contrasting colors)
+      def say_list(list)
+        list.each_with_index do |item,index|
+          color = index.even? ? :white : :cyan
+          say("#{index} : #{item}",color)
+        end
+      end
+
     end
 
   end
--- test/runner_test.rb
+++ test/runner_test.rb
@@ -8,8 +8,8 @@ context "The Dhun::Runner" do
   end
 
   context "start task" do
-    should("daemonize the server") do
-      capture(:stdout) { @runner.start(['start_server','-d']) }
+    should("start the server") do
+      capture(:stdout) { @runner.start(['start_server']) }
     end.matches(/Starting Dhun/)
   end
 
@@ -41,4 +41,102 @@ context "The Dhun::Runner" do
     end.matches(/first/)
   end
 
+  context "play task" do
+    context "with no search query" do
+      setup { mock.instance_of(@runner).return_response(:play,[]) { true } }
+      should("play the song") { capture(:stdout) {@runner.start(['play'])} }.equals ""
+    end
+    context "with search query" do
+      setup do
+        mock.instance_of(@runner).return_response(:clear,[]) { true }
+        mock.instance_of(@runner).invoke(anything,anything) { true }
+      end
+      should("call clear and enqueue") {  capture(:stdout) {@runner.start(['play','testing']) } }.equals ""
+    end
+  end
+  
+  context "enqueue task" do
+    setup { mock.instance_of(@runner).invoke(anything,anything) { ['one','two'] } }
+    context "with no search query" do
+      asserts("returns nothing") { capture(:stdout) { @runner.start(['enqueue']) } }.equals ''
+    end
+    context "with search query" do
+      setup do
+        
+      end
+    end
+  end
+    
+  context "status task" do
+    context "with response" do
+      setup do
+        mock.instance_of(@runner).server_running? { true }
+        mock.instance_of(@runner).return_response(:status,[:current,:queue]) { {:current => 'one', :queue => ['two']}}
+      end
+      asserts("says Currently Playing Playing") { capture(:stdout) { @runner.start(['status']) } }.matches(/Currently Playing/)
+      asserts("says Queue") { capture(:stdout) { @runner.start(['status']) } }.matches(/Queue/)
+      should("show current song") { capture(:stdout) { @runner.start(['status']) } }.matches(/one/)
+      should("show queue") { capture(:stdout) { @runner.start(['status']) } }.matches(/two/)
+    end
+    context "without response" do
+      setup { mock.instance_of(@runner).return_response(:status,[:current,:queue]) { false } }
+      asserts("returns false") { @runner.start(['status']) }.equals false
+    end
+  end
+  
+  context "history task" do
+    context "with response" do
+      setup do
+        mock.instance_of(@runner).server_running? { true }
+        mock.instance_of(@runner).return_response(:history,[:history]) { {:history => ['two']}}
+      end
+      asserts("says History") { capture(:stdout) { @runner.start(['history']) } }.matches(/History/)
+      should("show history") { capture(:stdout) { @runner.start(['history']) } }.matches(/two/)
+    end
+    context "without response" do
+      setup { mock.instance_of(@runner).return_response(:history,[:history]) { false } }
+      asserts("returns nothing") { @runner.start(['history']) }.equals false
+    end
+  end
+  
+  context "shuffle task" do
+    context "with response" do
+      setup do
+        mock.instance_of(@runner).server_running? { true }
+        mock.instance_of(@runner).return_response(:shuffle,[:queue]) { {:queue => ['two']}}
+      end
+      asserts("says Queue") { capture(:stdout) { @runner.start(['shuffle']) } }.matches(/Queue/)
+      should("show queue") { capture(:stdout) { @runner.start(['shuffle']) } }.matches(/two/)
+    end
+    context "without response" do
+      setup { mock.instance_of(@runner).return_response(:shuffle,[:queue]) { false } }
+      asserts("returns nothing") { @runner.start(['shuffle']) }.equals false
+    end
+  end
+
+  context "next task" do
+    setup { mock.instance_of(@runner).return_response(:next,[],1) { true } }
+    asserts("sends next command") { capture(:stdout) { @runner.start(['next']) { true } } }.equals ''
+  end
+  
+  context "prev task" do
+    setup { mock.instance_of(@runner).return_response(:prev,[],1) { true } }
+    asserts("sends next command") { capture(:stdout) { @runner.start(['prev']) { true } } }.equals ''
+  end
+
+  context "pause task" do
+    setup { mock.instance_of(@runner).return_response(:pause,[]) { true } }
+    asserts("sends pause command") { capture(:stdout) { @runner.start(['pause']) { true } } }.equals ''
+  end
+  
+  context "resume task" do
+    setup { mock.instance_of(@runner).return_response(:resume,[]) { true } }
+    asserts("sends resume command") { capture(:stdout) { @runner.start(['resume']) { true } } }.equals ''
+  end
+  
+  context "stop task" do
+    setup { mock.instance_of(@runner).return_response(:stop,[]) { true } }
+    asserts("sends stop command") { capture(:stdout) { @runner.start(['stop']) { true } } }.equals ''
+  end
+  
 end
